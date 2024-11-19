84f5ecbe484a65655ac6104f90337a7d6988ef8a
rinku090909 <tejveersingh6735@gmail.com>
2024-11-19 08:33:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,14 @@
 # Don't use quotes( " and ' )
-#SCRIPT BY VENOMxCRAZY
+#SCRIPT BY rinku090909
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7259034553:AAHYWdbEpMxeJAkv_HH1QYOChdMLMvot13E")
+BOT_TOKEN=("7385728236:AAFZKQz3WItKjBiKNvMOpP5KvDw68DrtmVI")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("tejveer0909")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
-
-
+ADMIN_IDS=("643217421991")
 
 
 
